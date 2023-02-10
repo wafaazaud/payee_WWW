c363a460270c3382e75ec1229de760a0e6888859
wafaazaud <118931414+wafaazaud@users.noreply.github.com>
2023-02-10 22:40:36 +0300

    Add files via upload

---
+++ Payee.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Charities_System_Database
+{
+    public partial class Payee : Form
+    {
+        B.CL_Payee select = new B.CL_Payee();
+        FM.EditPayee a = new FM.EditPayee();
+        public Payee()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Payee_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = select.GET_ALL_Payee();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FM.AddPayee add = new FM.AddPayee();
+            add.ShowDialog();
+            dataGridView1.DataSource = select.GET_ALL_Payee();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            a.ShowDialog();
+            dataGridView1.DataSource = select.GET_ALL_Payee();
+        }
+        
+        private void button5_Click(object sender, EventArgs e)
+        {
+            B.CL_Payee payee = new B.CL_Payee();
+            try
+            {
+                if (dataGridView1.Rows.Count == 0) { MessageBox.Show("لا يوجد بيانات لحذفها", "عملية الحذف", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+                else if (MessageBox.Show("هل تريد فعلا حذف المنتفع المحدد؟", "عملية الحذف", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) == DialogResult.Yes)
+                {
+                    payee.DeletePayee(Convert.ToInt32(this.dataGridView1.CurrentRow.Cells[0].Value.ToString()));
+                    MessageBox.Show("تمت عملية الحذف بنجاح", "عملية الحذف", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.dataGridView1.DataSource = payee.GET_ALL_Payee();
+                }
+                else
+                {
+                    MessageBox.Show("تم إلغاء عملية الحذف", "عملية الحذف", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+            catch { return; };
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                a.txtno.Text = this.dataGridView1.CurrentRow.Cells[0].Value.ToString();
+                a.txtname.Text = this.dataGridView1.CurrentRow.Cells[1].Value.ToString();
+                a.txtg.Text = this.dataGridView1.CurrentRow.Cells[2].Value.ToString();
+                //a.dateTP.Text = dataGridView1.CurrentRow.Cells[6].Value.ToString();
+                a.txtnumcard.Text = this.dataGridView1.CurrentRow.Cells[3].Value.ToString();
+                a.txtadderss.Text = this.dataGridView1.CurrentRow.Cells[4].Value.ToString();
+                a.txtphone.Text = this.dataGridView1.CurrentRow.Cells[5].Value.ToString();
+                a.txtw.Text = this.dataGridView1.CurrentRow.Cells[7].Value.ToString();
+                a.txtS.Text = this.dataGridView1.CurrentRow.Cells[8].Value.ToString();
+                a.txtnot.Text = this.dataGridView1.CurrentRow.Cells[9].Value.ToString();
+                a.combclass.Text = this.dataGridView1.CurrentRow.Cells[10].Value.ToString();
+
+            }
+            catch
+            {
+
+
+            } 
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                DL.DataAccessLayer d = new DL.DataAccessLayer();
+                SqlDataAdapter sdar = new SqlDataAdapter("select P_Id'رقم المنتفع', P_Name'اسم المنتفع',P_Gender'الجنس',P_CardNum'رقم البطاقة',P_Address'العنوان',P_NumPhone'رقم الهاتف ',P_AddDate'تاريخ الأضافة',P_WivesNum'عدد الزوجات',P_SonsNum'عدد الأبناء',P_Notes'ملاحظات',Codification'المجموعة/التصنيف' from Payee p ,Codifications c where p.C_Id=c.C_Id  and Convert (nvarchar,P_Id)+P_name+P_Address+P_CardNum+P_NumPhone+Convert (nvarchar,P_Gender) like '%" + txtsearsh.Text + "%'", d.sqlconn);
+                DataTable dt = new DataTable();
+                dt.Clear();
+                sdar.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            catch { return; };
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Payee.designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Charities_System_Database
+{
+    partial class Payee
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtsearsh = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtsearsh);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1222, 108);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+           // this.pictureBox1.Image = global::Charities_System_Database.Properties.Resources.loupe_research_zoom_magnifier_browse_find_search_glass_magnifying_icon_210803;
+            this.pictureBox1.Location = new System.Drawing.Point(165, 53);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(46, 37);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Window;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label2.ForeColor = System.Drawing.Color.Teal;
+            this.label2.Location = new System.Drawing.Point(1017, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "أبحث عن...";
+            // 
+            // txtsearsh
+            // 
+            this.txtsearsh.Location = new System.Drawing.Point(217, 70);
+            this.txtsearsh.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtsearsh.Name = "txtsearsh";
+            this.txtsearsh.Size = new System.Drawing.Size(795, 20);
+            this.txtsearsh.TabIndex = 4;
+            this.txtsearsh.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Window;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.ForeColor = System.Drawing.Color.Teal;
+            this.label1.Location = new System.Drawing.Point(492, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "قائمة المنتفعين";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F);
+            this.groupBox1.Location = new System.Drawing.Point(0, 108);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.groupBox1.Size = new System.Drawing.Size(1222, 486);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "فرز وأستعلام";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 18);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 26;
+            this.dataGridView1.Size = new System.Drawing.Size(1216, 466);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 513);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1222, 81);
+            this.panel2.TabIndex = 2;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.Window;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+           // this.button5.Image = global::Charities_System_Database.Properties.Resources.user_delete_114455;
+            this.button5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button5.Location = new System.Drawing.Point(782, 11);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(147, 61);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "حذف";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.Window;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+          //  this.button3.Image = global::Charities_System_Database.Properties.Resources.update_reload_sync_refresh_retry_icon_208184;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.Location = new System.Drawing.Point(945, 11);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(147, 61);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "تعديل";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Window;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+           // this.button2.Image = global::Charities_System_Database.Properties.Resources.user_add_114450;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(1107, 11);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 61);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "أضافة";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Window;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+           // this.button1.Image = global::Charities_System_Database.Properties.Resources.finishflag_acabado_3661;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(5, 11);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 61);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "أنهاء";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Payee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1222, 594);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Payee";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Payee";
+            this.Load += new System.EventHandler(this.Payee_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtsearsh;
+    }
+}
